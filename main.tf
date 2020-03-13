4fdbcfb2c081e02583cd940ec0bdf2fe771f717c
Marzo <andremarzo@gmail.com>
2020-03-13 16:26:42 -0300

    :sparkles: Work files

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region                  = "us-east-2"
   version                 = "~> 2.0"
   shared_credentials_file = "/home/marzo/.aws/credentials"
-  profile                 = "terraform_teste"
+  profile                 = "terraform"
 }
 
 resource "aws_vpc" "vpc_core" {
@@ -19,5 +19,18 @@ resource "aws_subnet" "dmz_t_a" {
 
   tags = {
     Name = "dmz_t_a"
+  }
+}
+
+resource "aws_route_table" "route_p_t_a" {
+  vpc_id = "${aws_vpc.vpc_core.id}"
+
+  route {
+    cidr_block = "10.1.1.0/24"
+    gateway_id = "${aws_internet_gateway.gateway_t_a.id}"
+  }
+
+  tags = {
+    Name = "route_p_t_a"
   }
 }
